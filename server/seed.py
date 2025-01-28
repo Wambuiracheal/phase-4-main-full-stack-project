656c22af2a4c31f7b939b2fdfc684a727c1d85f9
Racheal <rachealnjugush@gmail.com>
2025-01-28 12:59:50 +0300

    add users

--- server/seed.py
+++ server/seed.py
@@ -1,5 +1,13 @@
 from app import app
-from models import db, Category, Artwork
+from models import db, Category, Artwork, User
+
+users = {
+    {"id": 1, "email": "anne@gmail.com", "name": "Anne Faith"},
+    {"id": 2, "email": "mike@gmail.com", "name": "Anthony Mike"},
+    {"id": 3, "email": "alex@gmail.com", "name": "Alex Luke"},
+    {"id": 4, "email": "John@gmail.com", "name": "Nicholas John"},
+    {"id": 5, "email": "Caroline@gmail.com", "name": "Caroline John"},
+}
 
 categories = [
     {"name": "Painting"},
@@ -14,36 +22,48 @@ artworks = [
     {"artist": "Ansel Adams", "title": "Moonrise", "image": "moonrise.jpg", "price": 1200, "category_name": "Photography"},
 ]
 
+def seed():
+    with app.app_context():
+
+        db.session.query(User).delete()
+        db.session.query(Artwork).delete()
+        db.session.query(Category).delete()
+        db.session.commit()
 
 
-# def seed_data():
-#     with app.app_context():
-    
-#         db.session.query(Artwork).delete()
-#         db.session.query(Category).delete()
-#         db.session.commit()
+        category_objects = {}
 
-#         category_objects = {}
-#         for category_data in categories:
-#             category = Category(name=category_data["name"])
-#             db.session.add(category)
-#             category_objects[category_data["name"]] = category
+        # users
+        for user_data in users:
+            user = User(email=user_data["email"], name=user_data["name"])
+            db.session.add(user)
 
-#         db.session.commit()
+        db.session.commit()
+        print("Added users successfully!")
 
-#         for artwork_data in artworks:
-#             category = category_objects[artwork_data["category_name"]]
-#             artwork = Artwork(
-#                 artist=artwork_data["artist"],
-#                 title=artwork_data["title"],
-#                 image=artwork_data["image"],
-#                 price=artwork_data["price"],
-#                 category_id=category.id
-#             )
-#             db.session.add(artwork)
+        # categories
+        for category_data in categories:
+            category = Category(name=category_data['name'])
+            db.session.add(category)
+            category_objects[category_data['name']] = category
 
-#         db.session.commit()
-#         print("Database seeded successfully!")
+        db.session.commit()
+        print(f'Added categories successfully!')
 
-# if __name__ == "__main__":
-#     seed_data()
+        # artworks
+        for artwork_data in artworks:
+            category = category_objects[artwork_data["category_name"]]
+            artwork = Artwork(
+                artist=artwork_data["artist"],
+                title=artwork_data["title"],
+                image=artwork_data["image"],
+                price=artwork_data["price"],
+                category_id=category.id
+            )
+            db.session.add(artwork)
+
+        db.session.commit()
+        print("Database seeded successfully!")
+
+if __name__ == "__main__":
+    seed()
