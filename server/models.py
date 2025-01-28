deed0d06c9ecdaf27d759b8b09464863af547649
Racheal <rachealnjugush@gmail.com>
2025-01-28 12:29:50 +0300

    create models

---
+++ server/models.py
@@ -0,0 +1,45 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    # relationships
+    exhibitions = db.relationship('Exhibition', back_populates='users', lazy=True)
+
+class Category(db.Model, SerializerMixin):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    
+    # relationships
+    artworks = db.relationships('Artwork', back_populates='categories', lazy=True)
+
+class Artwork(db.Model, SerializerMixin):
+    __tablename__ = 'artworks'
+    id = db.Column(db.Integer, primary_key=True)
+    artist = db.Column(db.String(120), nullable=False)
+    title = db.Column(db.String(120), nullable=False)
+    image = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    category_id = db.Column(db.Integer(120), db.ForeignKey(Category.id))
+
+    # relationships
+    exhibitions = db.relationship('Exhibition', backref='artwork', lazy=True)
+
+class Exhibition(db.Model, SerializerMixin):
+    __tablename__ = 'exhibitions'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+    artwork_id = db.Column(db.Integer, db.ForeignKey(Artwork.id))
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+
+    
