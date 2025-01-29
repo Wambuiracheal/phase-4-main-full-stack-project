c0f509b75b10dfb24973d5e38021e112f565c15f
Racheal <rachealnjugush@gmail.com>
2025-01-29 18:11:35 +0300

    routes

--- client/artwork-app/main.jsx
+++ client/artwork-app/main.jsx
@@ -1,8 +1,37 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-// import Contact from './components/Contact.jsx'
-// import About from './components/About.jsx'
-// import Layout from './components/Layout.jsx'
-// import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Contact from './components/Contact.jsx'
+import About from './components/About.jsx'
+import Layout from './components/Layout.jsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+const route = createBrowserRouter([
+    {
+        path:'/',
+        element: <Layout />,
+        children:[
+            {
+            path:'/',
+            element: <App />
+            },
+            {
+                path:'/about',
+                element: <About />
+            },
+            {
+                path:'/contact',
+                element: <Contact />
+            }
+
+        ]
+    }
+
+])
+
+    createRoot(document.getElementById('root')).render(
+    <StrictMode>
+      <RouterProvider router = {router}/>
+    </StrictMode>,
+    )
